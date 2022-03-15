b1d06b88aa426af1a75ee59dac797a0377e6784f
andeusse <andeusse@gmail.com>
2022-03-15 15:33:01 -0500

    Recipe detail feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -23,17 +23,23 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializaer for recipe objects"""
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
 
     class Meta:
-        ingredients = serializers.PrimaryKeyRelatedField(
-            many=True,
-            queryset=Ingredient.objects.all()
-        )
-        tags = serializers.PrimaryKeyRelatedField(
-            many=True,
-            queryset=Tag.objects.all()
-        )
         model = Recipe
         fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
                   'price', 'link')
         read_only_fields = ('id', )
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
