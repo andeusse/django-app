94b5955b0372421ef0e96339f666e918760aeb23
andeusse <andeusse@gmail.com>
2022-03-15 09:52:16 -0500

    Added create ingredient feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,3 +38,7 @@ class IngredientViewSet(
     def get_queryset(self):
         """Return ingredient for the authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create new ingredient"""
+        serializer.save(user=self.request.user)
